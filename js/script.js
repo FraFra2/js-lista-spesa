3b9889a6705bac6e53e6e7e76955b5d4e4534f7a
FraFra2 <kekkino41@gmail.com>
2023-06-19 13:15:38 +0200

    Creazione Lista e Ciclo While

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let listaSpesa = [
+    "Latte",
+    "Uova",
+    "Pane",
+    "Frutta",
+    "Verdura",
+    "Carne",
+    "Pasta",
+    "Riso",
+    "Olio d'oliva",
+    "Sapone"
+  ];
+
+let myUl = document.createElement("ul");
+
+let c = 0;
+
+while (c < listaSpesa.length){
+    console.log(listaSpesa[c]);
+    c++;
+}
